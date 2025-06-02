284aecc3119b594a1e7baf9d470bda4c188c5936
Daniel Acquesta <dgacquesta@gmail.com>
2025-06-02 13:09:00 -0700

    added db reset command to package json

--- backend/prisma/migrations/20250602195850_/migration.sql
+++ backend/prisma/migrations/20250602195850_/migration.sql
@@ -1,10 +1,13 @@
 -- CreateTable
 CREATE TABLE "Changelog" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "repoOwner" TEXT NOT NULL,
     "project" TEXT NOT NULL,
     "from" TEXT NOT NULL,
     "to" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
     "content" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Changelog_pkey" PRIMARY KEY ("id")
 );
